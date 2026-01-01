0eec52e919edc389d72f2c100355e5366a524973
Jefik37 <58911866+Jefik37@users.noreply.github.com>
2026-01-01 18:17:04 -0300

    Delete preload.js

--- preload.js
+++
@@ -1,9 +0,0 @@
-(function() {
-    for(let i = 0; i < 10; i++) {
-        const img = new Image();
-        img.src = `./assets/lcd/${i}.png`;
-    }
-    
-    const dash = new Image();
-    dash.src = `./assets/lcd/-.png`;
-})();
